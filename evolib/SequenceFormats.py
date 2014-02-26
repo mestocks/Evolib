8a5c350e6bd140df4a40d3f21012d4b3c6247e67
Michael Stocks <ms@mspopgen.com>
2014-02-26 17:53:19 +0000

    Added fasta class

--- evolib/SequenceFormats.py
+++ evolib/SequenceFormats.py
@@ -122,3 +122,101 @@ class VariantCallFormat():
         
         return column_list
 """
+
+class FastaFormat():
+    """
+    Example usage:
+    
+       >>> from evolib.SequenceFormats import FastaFormat
+       
+       Example 1:
+       >>> fileObject = open('example.fsa', 'r')
+       >>> F = FastaFormat(fileObject)
+       
+       Example 2:
+       >>> seq = 'ATCTGATGCTGAC'
+       >>> F = FastaFormat(seq)
+       
+       Example 3:
+       >>> id = 'seq1'
+       >>> seq = 'ATCGATGTCGTGAC'
+       >>> F = FastaFormat(seq, id)
+       
+       Example 4:
+       >>> ids = ['seq1', 'seq2', 'seq3']
+       >>> seqs = ['ATCGATGTCGTGAC', 'ATCGATGTCGTGAC', 'ATCGATGTCGTGAC']
+       >>> F = FastaFormat(seqs, ids)
+    """
+
+    def __init__(self, *args):
+        
+        if len(args) == 1:
+            arg1 = args[0]
+            if isinstance(arg1, file):
+                self._fromFile(arg1)
+            elif isinstance(arg1, str):
+                self._fromSequence(None, [arg1])
+            elif isinstance(arg1, list):
+                self._fromSequence(None, arg1)
+            else:
+                raise TypeError, 'Wrong arg type. File object or list of sequences.'
+        elif len(args) == 2:
+            arg1 = args[0]
+            arg2 = args[1]
+            if isinstance(arg1, str):
+                assert isinstance(arg2, str)
+                self._fromSequence([arg2], [arg1])
+            elif isinstance(arg1, list):
+                assert isinstance(arg2, list)
+                assert len(arg1) == len(arg2)
+                self._fromSequence(arg2, arg1)
+            else:
+                raise TypeError, 'Wrong arg type. File object or list of sequences.'
+            
+    def __str__(self):
+        n = 70
+        the_string = ''
+        for s in range(len(self.sequences)):
+            seq = self.sequences[s]
+            the_string += '>' + self.ids[s] + '\n'
+            the_string += '\n'.join([seq[i: i + n] for i in range(0, len(seq), n)]) + '\n'
+            
+        return the_string.rstrip()
+
+    def _fromFile(self, fileObject):
+        """
+        Returns a matrix where M[i][j] refers to the jth site of the ith individual.
+        """        
+        step1 = ''.join(map(lambda line: line, fileObject))
+        step2 = step1.split('\n>')
+        seq_table = [part.partition('\n')[2].replace('\n','') for part in step2]
+        seq_names = [part.partition('\n')[0].replace('>', '') for part in step2]
+        
+        self.sequences = seq_table
+        self.ids = seq_names
+
+    def _fromSequence(self, ids, seqs):
+        
+        if ids is None:
+            nseqs = len(seqs)
+            ids = self._create_ids(nseqs)
+        
+        self.ids = ids
+        self.sequences = seqs
+
+    def _create_ids(self, nseqs):
+        """
+        Creates a list of sequence ids equal to the number 
+        of sequences as shown in the examples below:
+           ['seq1', 'seq2', 'seq3']
+           ['seq01', 'seq02', ...'seq32']
+        """
+        ids = []
+        for i in range(nseqs):
+            seqnum = i + 1
+            num_zeros = len(str(nseqs)) - len(str(seqnum))
+            zeros = '0' * num_zeros
+            id1 = 'seq' + zeros + str(seqnum)
+            ids.append(id1)
+            
+        return ids
