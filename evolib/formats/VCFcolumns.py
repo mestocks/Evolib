6fb460f239f874368cf82f1bd0cd961f2a0be1e8
Michael Stocks <ms@mspopgen.com>
2015-03-16 21:50:34 -0700

    Update VCFcolumns.py

--- evolib/formats/VCFcolumns.py
+++ evolib/formats/VCFcolumns.py
@@ -29,9 +29,12 @@ class COL_BASECLASS(object):
 
 #######################
 
-class CHROM(COL_BASE_STR):
+class CHROM_old(COL_BASE_STR):
     #__slots__ = ['col_name', 'chr_value', 'value']
     col_name = 'CHROM'
+
+class CHROM(str):
+    pass
 
 class POS(COL_BASE_INT):
     __slots__ = ['col_name', 'chr_value']
