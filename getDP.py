eef6b126d7f28aedd0f1b44bf4e6ba94b0ada200
Michael Stocks <mspopgen@gmail.com>
2015-04-01 15:08:04 +0100

    diag

--- getDP.py
+++ getDP.py
@@ -4,11 +4,11 @@ from evolib.iterators import vcf_iter
 
 for row in vcf_iter(sys.stdin):
             
-    dp = list((smp['DP'] for smp in row.iter_samples()))
+    dp = map(str, (smp['DP'] for smp in row.iter_samples()))
 
     chrom, pos = row['CHROM'], row['POS']
 
-    print ' '.join([chrom, str(pos)] + dp) 
+    print ' '.join([chrom, str(pos)]), ' '.join(dp) 
         
         #for i in pop1:
         #    printstr += i,
