8a2cc59855edbe293c20f530443ff8d43941a34c
Michael Stocks <mspopgen@gmail.com>
2015-04-15 15:44:54 +0100

    faenon

--- diag_fae_non.py
+++ diag_fae_non.py
@@ -79,6 +79,6 @@ for row in vcf_iter(sys.stdin):
             #print hom1, het1, alt1
             #print hom2, het2, alt2
             print row
-            print '\t'.join(printlst)
+            #print '\t'.join(printlst)
             
     
