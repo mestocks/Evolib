6199609905285c3b6859717c9a1781b916a811a4
Michael Stocks <mspopgen@gmail.com>
2015-04-15 15:00:55 +0100

    faenon

--- diag_fae_non.py
+++ diag_fae_non.py
@@ -59,7 +59,7 @@ for row in vcf_iter(sys.stdin):
     het2 = pop2.count('0/1')
     alt2 = pop2.count('1/1')
 
-    if n1 == 2 and n2 == 3 and (n1 + n2) == sum([hom1, het1, alt1, hom2, het2, alt2]):
+    if n1 == len(names1) and n2 == len(names2) and (n1 + n2) == sum([hom1, het1, alt1, hom2, het2, alt2]):
         # fae/non-fae conditions
         # ((0/0 + 0/1 > 0) and (1/1 + 0/1 == 0)) or ((1/1 + 0/1 > 0) and (0/0 + 0/1 == 0))
         if ((hom1 + het1 > 0) and (hom2 + het2 == 0)) or ((alt1 + het1 > 0) and (alt2 + het2 == 0)):
