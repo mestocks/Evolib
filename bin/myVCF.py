7d76c42692cf2353f361550d34d8bbbb1af6f194
Michael Stocks <mspopgen@gmail.com>
2015-06-09 14:55:22 +0000

    myvcf

--- bin/myVCF.py
+++ bin/myVCF.py
@@ -1,3 +1,26 @@
-
+"""
+...| python myVCF.py <col1>,<col2>
+"""
 
 import sys
+
+args = sys.argv[1:]
+
+from evolib.iterators import vcf_iter
+
+colstr = args[0]
+cols = colstr.split(",")
+
+for row in vcf_iter(sys.stdin):
+    chrom, pos = row["CHROM"], row["POS"]
+
+    print chrom, pos,
+
+    for col in cols:
+        colobj = row[col]
+        print colobj,
+    print "\n",
+
+    
+
+    
