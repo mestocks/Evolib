261714174840c8df3f8629b2371ca3bae594884a
Michael Stocks <ms@mspopgen.com>
2015-07-01 11:41:19 +0100

    Update het.py

--- bin/het.py
+++ bin/het.py
@@ -7,9 +7,21 @@ vcf = VariantCallFormat(sys.stdin)
 args = sys.argv[1:]
 names = args[0].split(",")
 
+def gtiter(row, names):
+    for samp in row.iter_samples():
+        if samp.name in names:
+            dp = samp['DP']
+            if dp != "." and dp is not None and int(dp) > 7:
+                gt = samp['GT']
+                if gt is None or gt == "./.":
+                    yield '0/0'
+                else:
+                    yield gt
+
 for row in vcf:
     chrom, pos = row['CHROM'], row['POS']
-    GTs = list((smp['GT'] for smp in row.iter_samples() if smp.name in names and smp['GT'] is not None and smp['GT'] != "./." and smp['DP'] != "." and smp['DP'] is not None and int(smp['DP']) > 7))
+    GTs = list(gtiter(row, names))
+    #GTs = list((smp['GT'] for smp in row.iter_samples() if smp.name in names and smp['GT'] is not None and smp['GT'] != "./." and smp['DP'] != "." and smp['DP'] is not None and int(smp['DP']) > 7))
     
     ngts = len(GTs)
     nhet = GTs.count("0/1")
